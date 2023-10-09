ee58416693635b8efe9e28b339e87e59ac378cd5
shubham <shubya8451@gmail.com>
2023-10-09 21:15:31 +0530

    redefined code into functions

--- server/main.py
+++ server/main.py
@@ -1,17 +1,47 @@
-from flask import Flask, json
+
+import requests
+import scraper as sp
+from flask import Flask, json, request
 from transformers import pipeline
+from bs4 import BeautifulSoup
 nlp = pipeline('question-answering', model='deepset/roberta-base-squad2', tokenizer='deepset/roberta-base-squad2')
 
-app = Flask(__name__)
+url ="""https://www.amazon.in/Crucial-3200MHz-2933MHz-2666MHz-CT8G4SFRA32A/dp/B08C4Z69LN/?_encoding=UTF8&pd_rd_w=asRZX&content-id=amzn1.sym.aff93425-4e25-4d86-babd-0fa9faf7ca5d%3Aamzn1.symc.36bd837a-d66d-47d1-8457-ffe9a9f3ddab&pf_rd_p=aff93425-4e25-4d86-babd-0fa9faf7ca5d&pf_rd_r=3S93CHJY45SQRCD4R7DC&pd_rd_wg=d59M2&pd_rd_r=ae6fcfa5-1b50-4c83-a0ee-f2a9eb1e8361&ref_=pd_gw_ci_mcx_mr_hp_atf_m&th=1"""
 
-context = """The name of the product is Crucial RAM 8GB DDR4 3200MHz CL22 (or 2933MHz or 2666MHz) Laptop Memory CT8G4SFRA32A . 
-It's Price is 1,499.Some specific details are as Brand is Crucial,Computer Memory Size is 8 GB, RAM Memory Technology is DDR4 ,Memory Speed is 3200 MHz, 
-Compatible Devices is Laptop, Features are  Improve your system's responsiveness, run apps faster and multitask with ease. Extended timings - 22-22-22 
-Install with ease; no computer skills required How-to guides available at Crucial   Compatibility assurance when using the Crucial System Scanner or C
-rucial Advisor Tool   Micron quality and reliability is backed by superior component and module level testing and 42 years of memory expertise  
-ECC Type Non-ECC, Form Factor SODIMM, Pin Count 260-pin, PC Speed PC4-25600, Voltage 12V, Rank and Configuration 1Rx16, 1Rx8 or 2Rx8.NOTE: 
-DRAM modules require configuration to specific systems  .Can be deliveredFREE delivery Wednesday, 11 October..Fastest Delivery by Fastest delivery tomorrow, 
-9 october. order within 19 hrs 55 mins..Stock Availablity = In stock.Seller information Sold by Appario Retail Private Ltd and Fulfilled by Amazon."""
+headers = {'authority': 'www.amazon.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'}
+
+
+# get url result html code here 
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+frog = soup.text
+
+# Summarizer Url
+summarize_endpoint = url.replace("/dp/","/product-reviews/")+f"?pageNumber={1}"
+
+# Critical Review
+critical_review = url.replace("/dp/","/product-reviews/")+"&filterByStar=critical"
+
+# Positive Review
+positive_review = url.replace("/dp/","/product-reviews/")+"&filterByStar=positive"
+
+
+
+
+
+
+app = Flask(__name__)
 
 
 
@@ -19,6 +49,27 @@ DRAM modules require configuration to specific systems  .Can be deliveredFREE de
 @app.route("/home", methods=["GET", "POST"])
 def home():
     return "Hello, Welcome to home."
+
+
+@app.route("/getBasicData", methods=["POST"])
+def getBasicDataRoute():
+    body = request.get_json()
+    print(body)
+    text = sp.getBasicData(soup=soup)
+    nlp({
+        'question': body['question'],
+        "context": text 
+    })
+
+    response = app.response_class(
+        response = json.dumps({
+            "status" : 200,
+            "message" : nlp['answer']
+        })
+    )
+
+    return response
+    
 
 @app.route("/questionAsk", methods=["POST"])
 def questionAsk():
